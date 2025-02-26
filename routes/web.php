ea82ead42a9fc85b9bc712ef2eac2a77a1a73b7d
mikaku23 <haliqksp@gmail.com>
2025-02-26 13:07:44 +0700

    ADD FEAT DASHBOARD

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\siswacontroller;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/dashboard', function () {
+    return view('index');
+});
+Route::fallback(function () {
+    return response()->view('eror', [], 404);
+});
+
+Route::resource('siswa', siswacontroller::class);
