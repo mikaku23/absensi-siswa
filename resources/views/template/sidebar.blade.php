003fe302d18f3b50a27bc4aef3af2c2178b7a2d5
mikaku23 <haliqksp@gmail.com>
2025-03-09 21:50:03 +0700

    FIX ICON SIDEBAR

--- resources/views/template/sidebar.blade.php
+++ resources/views/template/sidebar.blade.php
@@ -1,32 +1,31 @@
-
 <aside id="sidebar" class="sidebar">
 
-     <ul class="sidebar-nav" id="sidebar-nav">
+    <ul class="sidebar-nav" id="sidebar-nav">
 
-         <li class="nav-item">
-             <a class="nav-link {{ $menu == 'home' ? '' : 'collapsed' }}" href="{{ route('home') }}">
-                 <i class="bi bi-grid"></i>
-                 <span>Dashboard</span>
-             </a>
-         </li>
-         <li class="nav-item">
-             <a class="nav-link {{ $menu == 'siswa' ? '' : 'collapsed' }}" href="{{ route('siswa.index') }}">
-                 <i class="bi bi-person"></i>
-                 <span>Siswa</span>
-             </a>
-         </li>
-         <li class="nav-item">
-             <a class="nav-link {{ $menu == 'guru' ? '' : 'collapsed' }}" href="{{ route('guru.index') }}">
-                 <i class="bi bi-person"></i>
-                 <span>Guru</span>
-             </a>
+        <li class="nav-item">
+            <a class="nav-link {{ $menu == 'home' ? '' : 'collapsed' }}" href="{{ route('home') }}">
+                <i class="bi bi-grid"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link {{ $menu == 'siswa' ? '' : 'collapsed' }}" href="{{ route('siswa.index') }}">
+                <i class="bi bi-person"></i>
+                <span>Siswa</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link {{ $menu == 'guru' ? '' : 'collapsed' }}" href="{{ route('guru.index') }}">
+                <i class="fas fa-chalkboard-teacher"></i>
+                <span>Guru</span>
+            </a>
         <li class="nav-item">
             <a class="nav-link {{ $menu == 'local' || $menu == 'jurusan' ? '' : 'collapsed' }}" href="{{ route('local.index') }}">
-                <i class="bi bi-person"></i>
+                <i class="bi bi-building"></i>
                 <span>Local</span>
             </a>
         </li>
 
-     </ul>
+    </ul>
 
- </aside><!-- End Sidebar-->
+</aside><!-- End Sidebar-->
